df8c771bcd71a7f8fff1ae140ea4c5a0ea6a98bb
Juliana Leon <jleon@homeaway.com>
2021-05-11 02:34:25 -0500

    Tutorial app

---
+++ BullsEyeTwo/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  BullsEyeTwo
+//
+//  Created by Juliana Leon on 10/28/20.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true , completion: nil)
+    }
+}
---
+++ BullsEyeTwo/AuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AuthorViewController.swift
+//  BullsEyeTwo
+//
+//  Created by Juliana Leon on 10/28/20.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back() {
+        dismiss(animated: true , completion: nil)
+    }
+
+}
